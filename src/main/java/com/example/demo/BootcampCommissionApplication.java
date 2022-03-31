6417061d7604d8777d85ebbc9aa4280d290e565e
jlamaspi <juliolamas4795@gmail.com>
2022-03-31 16:06:11 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/BootcampCommissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampCommissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampCommissionApplication.class, args);
+	}
+
+}
